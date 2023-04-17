de5aca474149389bc494e0949fc293bc91f3fa34
Weep-Lee <lzx_jxlg@163.com>
2023-04-17 23:54:05 +0800

    把logMapper补上

---
+++ src/main/java/com/cg/mapper/LogMapper.java
@@ -0,0 +1,18 @@
+package com.cg.mapper;
+
+import com.cg.pojo.Log;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author WeepLee
+* @description 针对表【t_log】的数据库操作Mapper
+* @createDate 2023-04-16 16:10:37
+* @Entity com.cg.pojo.Log
+*/
+public interface LogMapper extends BaseMapper<Log> {
+
+}
+
+
+
+
